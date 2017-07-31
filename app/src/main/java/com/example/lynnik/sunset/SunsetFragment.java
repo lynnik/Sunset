9b26cf68a4744a37b7b1543db920882b6355f780
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-31 10:03:02 +0300

    Animation is added.

--- app/src/main/java/com/example/lynnik/sunset/SunsetFragment.java
+++ app/src/main/java/com/example/lynnik/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.example.lynnik.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SunsetFragment extends Fragment {
+
+  private View mSceneView;
+  private View mSunView;
+  private View mSkyView;
 
   public static SunsetFragment newInstance() {
     return new SunsetFragment();
@@ -21,6 +26,27 @@ public class SunsetFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.fragment_sunset, container, false);
 
+    mSceneView = v;
+    mSunView = v.findViewById(R.id.sun);
+    mSkyView = v.findViewById(R.id.sky);
+
+    mSceneView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        startAnimation();
+      }
+    });
+
     return v;
+  }
+
+  private void startAnimation() {
+    float sunYStart = mSunView.getTop();
+    float sunYEnd = mSunView.getHeight();
+
+    ObjectAnimator heightAnimator = ObjectAnimator
+        .ofFloat(mSunView, "y", sunYStart, sunYEnd);
+
+    heightAnimator.start();
   }
 }
